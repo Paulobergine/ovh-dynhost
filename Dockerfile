54c7a6ee17e0d23429b8edadfa7132ad4a4e0bd8
Paul Guerre <pguerreftb@gmail.com>
2022-07-12 16:45:02 +0200

    Added support for list of domain name

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,15 @@
 FROM alpine:3.9
 
-RUN apk add --update --no-cache curl bind-tools
+RUN apk add --update --no-cache curl bind-tools jq dos2unix
 
 WORKDIR /usr/src/app
 
 COPY dynhost.sh .
+COPY DDNS.json .
+
 RUN chmod +x ./dynhost.sh
+
+RUN dos2unix -b ./dynhost.sh
 
 RUN ln -sf /usr/src/app/dynhost.sh /etc/periodic/15min/dynhost
 
--- dynhost.sh
+++ dynhost.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
-echo "Checking IP" >> dynhost.log
+DDNS=`cat DDNS.json | jq '.DDN'`
 
-IP=$(dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d \")
-CURRENT_IP=$(dig +short $DYNHOST_DOMAIN_NAME)
+for DDN in $DDNS
+do
+  echo "Checking IP" >> dynhost.log
 
-if [ "$IP" ]; then 
-    if [ "$CURRENT_IP" != "$IP" ]; then 
+  IP=$(dig -4 TXT +short o-o.myaddr.l.google.com @ns1.google.com | tr -d \")
+  CURRENT_IP=$(dig +short $DYNHOST_DOMAIN_NAME)
+
+  if [ "$IP" ]; then
+    if [ "$CURRENT_IP" != "$IP" ]; then
         echo "Updating $DYNHOST_DOMAIN_NAME from $CURRENT_IP to $IP" >> dynhost.log
         curl --user "${DYNHOST_LOGIN}:${DYNHOST_PASSWORD}" "http://www.ovh.com/nic/update?system=dyndns&hostname=${DYNHOST_DOMAIN_NAME}&myip=${IP}" >> dynhost.log 2>&1
-    else 
+    else
         echo "No update required" >> dynhost.log
-    fi 
-else 
+    fi
+  else
     echo "IP not found" >> dynhost.log
-fi
+  fi
+done
